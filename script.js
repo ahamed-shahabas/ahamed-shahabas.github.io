ccd7cb3007b10006f3a1699e8ee482ecf4222b47
AHAMED SHAHABAS K M <shahabas.it@protonmail.com>
2026-04-04 12:51:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// Typing Loop
+const roles = ["IT Support Engineer", "Network Specialist", "System Administrator"];
+let i = 0, j = 0, current = "", isDeleting = false;
+
+function type() {
+  current = roles[i];
+
+  if (!isDeleting) {
+    document.getElementById("typing").innerHTML = current.substring(0, j++);
+  } else {
+    document.getElementById("typing").innerHTML = current.substring(0, j--);
+  }
+
+  if (!isDeleting && j === current.length) {
+    isDeleting = true;
+    setTimeout(type, 1000);
+    return;
+  }
+
+  if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % roles.length;
+  }
+
+  setTimeout(type, isDeleting ? 50 : 100);
+}
+type();
+
+// Fix particles error if not loaded
+if (typeof particlesJS !== "undefined") {
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 60 },
+      size: { value: 3 }
+    }
+  });
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("light-mode");
+}
